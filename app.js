df08625a4b2bcbf6bce6c991e136c258ba8e9d6c
ColeyDohmen <coley.dawn12@gmail.com>
2021-02-15 15:11:48 -0700

    push

---
+++ app.js
@@ -0,0 +1,333 @@
+class Armadillo {
+    constructor(name, color, size, special, dirty)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.dirty = dirty;
+        }
+}
+let andy = new Armadillo(`Andy`, `gray`, `medium`,`rolly`, true)
+
+class Barracuda {
+    constructor(name, color, size, speed, special, aquatic)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.speed = speed;
+            this.special = special;
+            this.aquatic = aquatic
+        }
+}
+
+let barry = new Barracuda (`Barry`, `gray`, `large`, `quick`, `eats Nemo's mom`, true)
+
+class Chinchilla {
+    constructor(name, color, size, special, fluff)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.fluff = fluff;
+        }
+}
+
+let charlie = new Chinchilla (`Charlie`, `gray`, `small`, `cuddly`, `super`)
+
+class Dolphin {
+    constructor(name, color, size, special, aquatic)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.aquatic = aquatic;
+        }
+}
+let darryl = new Dolphin (`Darryl`, `gray`, `medium`, `deceiving`, true)
+
+class Eagle {
+    constructor(name, color, size, special, flight)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.flight = flight;
+        }
+}
+let ernie = new Eagle (`Ernie`, `brown`, `largeish`, `Merica`, true)
+
+class Frog {
+    constructor(name, color, size, special, slimy)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.slimy = slimy;
+        }
+}
+let frank = new Frog (`Frank`, `green`, `small`, `jumpy`, true)
+
+class Gecko {
+    constructor(name, color, size, special, slimy)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.slimy = slimy;
+        }
+}
+let greg = new Gecko(`Greg`, `blue`,`small`,`supacute`, true)
+
+class Hummingbird {
+    constructor(name, color, size, special, speed, flight)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.speed = speed;
+            this.flight = flight;
+        }
+}
+let harrison = new Hummingbird(`Harrison`, `multi`,`small`,`quick af`, `super`, true)
+
+class Ibis {
+    constructor(name, color, size, special, flight)
+    {
+        this.name = name;
+        this.color = color;
+        this.size = size;
+        this.special = special;
+        this.flight = flight;
+    }
+}
+let isiah = new Ibis(`Isiah`,`white`,`medium`,`birdy`, true)
+
+class Jellyfish {
+    constructor(name, color, size, special, aquatic, slimy)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.aquatic = aquatic;
+            this.slimy = slimy;
+        }
+}
+let jerry = new Jellyfish(`Jerry`, `translucent`,`small`,`jam`, true, true)
+
+class Kiwi {
+    constructor(name, color, size, special)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+        }
+}
+let kellen = new Kiwi(`Kellen`, `brown`, `small`, `NZ only`)
+
+class Lion {
+    constructor(name, color, size, special, ferocity)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.ferocity = ferocity;
+        }
+}
+let levi = new Lion(`Levi`, `yellow-orange`,`large`,`fiery`,100)
+
+class Mole {
+    constructor(name, color, size, special, dirty)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.dirty = dirty;
+        }
+}
+let mick = new Mole(`Mick`, `brown`, `small`, `dirty`, true)
+
+class Narwhal {
+    constructor(name, color, size, special, aquatic)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.aquatic = aquatic;
+        }
+}
+let ned = new Narwhal(`Ned`,`bluegray`,`medium`,`pokey`, true)
+
+class Ocelot {
+    constructor(name, color, size, special, speed)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.speed = speed;
+        }
+}
+let oscar = new Ocelot(`Oscar`,`yellow`,`small-medium`,`cute`,`kinda`)
+
+class Pangolin {
+    constructor(name, color, size, special)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+        }
+}
+let peter = new Pangolin(`Peter`, `gray-brown`,`small-medium`,`pokey`)
+
+class Quail {
+    constructor(name, color, size, special, flight)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.flight = flight;
+        }
+}
+let quentin = new Quail(`Quentin`, `brown`,`small`,`birdy`, true)
+
+class Raccoon {
+    constructor(name, color, size, special, dirty)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.dirty = dirty;
+        }
+}
+let rob = new Raccoon(`Rob`,`gray-black`,`small-medium`,`thief`, true)
+
+class Snail {
+    constructor(name, color, size, special,speed, slimy)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.speed = speed;
+            this.slimy = slimy;
+        }
+}
+let sammy = new Snail(`Sammy`,`green`,`small`,`slimy`,`slow`,true)
+
+class Tapir {
+    constructor(name, color, size, special, dirty)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.dirty = dirty;
+        }
+}
+let tim = new Tapir(`Tim`,`blackwhite`,`medium`,`weird-looking`,true)
+
+class Umbrellabird {
+    constructor(name, color, size, special, flight)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.flight = flight
+        }
+}
+let uma = new Umbrellabird(`Uma`,`black`,`small`,`birdy`, true)
+
+class Vampirebat {
+    constructor(name, color, size, special, flight)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+            this.flight = flight;
+        }
+}
+let vinny = new Vampirebat(`Vinny`,`black`,`small`,`bloodsucker`, true)
+
+class Wallaby {
+    constructor(name, color, size, special)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+        }
+}
+let wallace = new Wallaby(`Wallace`,`brown`,`small-medium`,`not a kangaroo`)
+
+class Xerus {
+    constructor(name, color, size, special)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+        }
+}
+let xavier = new Xerus(`Xavier`,`brown`,`small`,`rodent`)
+
+class Yak {
+    constructor(name, color, size, special)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+        }
+}
+let yoel = new Yak(`Yoel`,`brown`,`large`,`yakky`)
+
+class Zebu {
+    constructor(name, color, size, special)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+            this.special = special;
+        }
+}
+let zain = new Zebu(`Zain`,`brown`,`large`,`lumpy`)
+
+class Zoo {
+    constructor(){
+    this.aquatic = []
+    this.flight = []
+    this.slimy = []
+    this.dirty = []
+    }
+}
+
+let zoo = new Zoo()
+zoo.aquatic.push(barry,darryl,ned,jerry)
+zoo.flight.push(ernie, harrison, isiah,quentin, uma, vinny)
+zoo.slimy.push(frank,greg,jerry,sammy)
+zoo.dirty.push(andy, mick, rob, tim)
+zoo.aquatic.forEach(animal => console.log(animal.name))
+
+zoo.flight.forEach(animal => console.log(animal.name))
+
+zoo.slimy.forEach(animal => console.log(animal.name))
+
+zoo.dirty.forEach(animal => console.log(animal.name))
